063b1eec8482a57af0c31f2de64ad9d677a75dce
Rania <Rania@email.com>
2025-12-17 19:19:30 +0100

    issue13:Remove this unused import 'org.springframework.web.bind.annotation.CrossOrigin'

--- iventory-service/src/main/java/org/sid/iventoryservice/repository/ProductRespository.java
+++ iventory-service/src/main/java/org/sid/iventoryservice/repository/ProductRespository.java
@@ -3,9 +3,6 @@ package org.sid.iventoryservice.repository;
 import org.sid.iventoryservice.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.web.bind.annotation.CrossOrigin;
-
-
 
 @RepositoryRestResource
 public interface ProductRespository extends JpaRepository<Product,String>
