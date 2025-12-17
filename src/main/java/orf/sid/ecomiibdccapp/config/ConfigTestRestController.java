d2a94f0b4836f49ce6aecfc8dc100c41ae037691
Rania <Rania@email.com>
2025-12-17 19:35:10 +0100

    issue:Remove this field injection and use constructor injection instead

--- src/main/java/orf/sid/ecomiibdccapp/config/ConfigTestRestController.java
+++ src/main/java/orf/sid/ecomiibdccapp/config/ConfigTestRestController.java
@@ -1,7 +1,5 @@
 package orf.sid.ecomiibdccapp.config;
 
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,27 +8,30 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
-@RefreshScope //indique de ce controlleur va etre instancier une fois on mets refresh
+@RefreshScope//indique de ce controlleur va etre instancier une fois on mets refresh
 public class ConfigTestRestController {
-    @Value("${global.params.p1}")//pour injecter les valeurs
 
-    private String p1;
+    private final String p1;
+    private final String p2;
+    private final CustomerConfigParams customerConfigParams;
 
-    @Value("${global.params.p2}")
-    private String p2;
-
-    @Autowired
-    private CustomerConfigParams customerConfigParams;
+    public ConfigTestRestController(
+            @Value("${global.params.p1}") String p1,//pour injecter les valeurs
+            @Value("${global.params.p2}") String p2,
+            CustomerConfigParams customerConfigParams
+    ) {
+        this.p1 = p1;
+        this.p2 = p2;
+        this.customerConfigParams = customerConfigParams;
+    }
 
     @GetMapping("/testConfig1")
-
-    public Map<String,String> configTest(){
-        return Map.of("p1",p1,"p2",p2);
+    public Map<String, String> configTest() {
+        return Map.of("p1", p1, "p2", p2);
     }
 
     @GetMapping("/testConfig2")
-
-    public CustomerConfigParams configTest2(){
+    public CustomerConfigParams configTest2() {
         return customerConfigParams;
     }
 }
