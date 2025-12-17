7cb8891997cee8db4e5b89ccf409d1b7f8869d50
Rania <Rania@email.com>
2025-12-17 21:00:50 +0100

    issue:Security Hotspots'Random'

--- billing-service/src/main/java/org/sid/billingservice/BillingServiceApplication.java
+++ billing-service/src/main/java/org/sid/billingservice/BillingServiceApplication.java
@@ -14,6 +14,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import java.security.SecureRandom;
 
 import java.util.Collection;
 import java.util.Date;
@@ -24,7 +25,10 @@ import java.util.Random;
 @EnableFeignClients
 public class BillingServiceApplication {
 
-    private static final Random RANDOM = new Random();
+
+
+    private static final SecureRandom RANDOM = new SecureRandom();
+
 
     public static void main(String[] args) {
         SpringApplication.run(BillingServiceApplication.class, args);
